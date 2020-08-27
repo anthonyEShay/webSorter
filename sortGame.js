54c0ce7c283f5440a271cc2b791ed4271be86f33
anthonyEShay <muymaverick@gmail.com>
2020-08-27 01:59:38 -0700

    good progress, stable

--- sortGame.js
+++ sortGame.js
@@ -1,23 +1,34 @@
 
-var redGamePiece, blueGamePiece, yellowGamePiece;
 
-function startGame() {
-    redGamePiece = new component(75, 75, "red", 10, 10);
-    yellowGamePiece = new component(75, 75, "yellow", 50, 60);    
-    blueGamePiece = new component(75, 75, "blue", 10, 110);
+var intervalTimer;
+var elementArray = [];
+var currentPos = 0;
+
+function startGame(intTime, numElements, density) {
+    intervalTimer = intTime;
+    elementArray = [];
+    currentPos = 0;
+    xPosition = Math.floor(density/2);
+    for (i = 0; i < numElements; i++){
+        yValue = Math.floor(Math.random()*500) + 1;
+        temp = new component(density, yValue, "green", xPosition, 0);
+        xPosition += density + Math.floor(density/2);
+        elementArray.push(temp);
+    }
     myGameArea.start();
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 720;
-        this.canvas.height = 480;
-        this.canvas.style.border = "1px solid";
+        this.canvas.width = 1100;
+        this.canvas.height = 500;
+        this.canvas.style.border = "2px solid";
         this.context = this.canvas.getContext("2d");
         div = document.getElementById("gameBox");
         div.appendChild(this.canvas);
-        this.interval = setInterval(updateGameArea, 20);
+        clearInterval(this.interval);
+        this.interval = setInterval(updateGameArea, intervalTimer);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -29,17 +40,36 @@ function component(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
-    this.update = function(){
-        this.x += 1
+    this.update = function(aBorder){
+        aString = " " + currentPos.toString();
+        document.getElementById("test2").innerHTML = aString;
+        
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        if(aBorder){
+            ctx.fillStyle = "red";
+            ctx.fillRect(this.x - 2, this.y, 2, this.height);
+            ctx.fillRect(this.x + this.width, this.y, 2, this.height);
+            ctx.fillRect(this.x - 2, this.height, this.width + 4, 2);
+        }
     }
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    redGamePiece.update();
-    yellowGamePiece.update();        
-    blueGamePiece.update();
+    aCount = 0;
+    for (x of elementArray){
+        if (aCount == currentPos){
+            x.update(true);
+        }else{
+            x.update(false);
+        }
+        aCount += 1;
+    }
+    currentPos += 1;
+    if (currentPos == elementArray.length){
+        currentPos = 0;
+    }
+    
 }
